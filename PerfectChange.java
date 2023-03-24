d8f1512863472548ce363486c7491ebdf20ee355
Music Anime <musicanime2@gmail.com>
2023-03-23 21:57:27 -0500

    structure of main created. working on Greedy solution

--- PerfectChange.java
+++ PerfectChange.java
@@ -1,9 +1,28 @@
-import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class PerfectChange
 {
-    static class Greedy
+    class Greedy
     {
+        private List<Integer> denominations;
+        private int target;
+
+        public Greedy(List<Integer> denominations, int target)
+        {
+            this.denominations = denominations;
+            this.target = target;
+        }
+
+        public boolean isGreedy()
+        {
+            denominations
+        }
+
+        public String toString()
+        {
+
+        }
 
     }
 
@@ -11,9 +30,37 @@ public class PerfectChange
     {
 
     }
-    
+
     public static void main(String[] args)
     {
-        System.out.println("works");
+        List<Integer> denominations = new ArrayList<>();
+        int target;
+
+        for(int i = 0; i < args.length; i++)
+        {
+            if(args[i] != 1)
+            {
+                denominations.add(Integer.parseInt(args[i]))
+            }
+            else
+            {
+                denominations.add(Integer.parseInt(args[i]));
+                target = Integer.parseInt(args[i + 1]);
+                break;
+            }
+        }
+
+        Greedy problem = new Greedy(denominations, target);
+
+        if(problem.isGreedy())
+        {
+            Greedy solution = problem;
+            System.out.println(solution);
+        }
+        else
+        {
+            Dynamic solution = new Dynamic(denominations, target);
+            System.out.println(solution);
+        }
     }
 }
