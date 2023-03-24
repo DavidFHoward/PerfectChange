39bb4a9990e5682c517df016604c043b94d41255
Music Anime <musicanime2@gmail.com>
2023-03-23 21:55:54 -0500

    first commit

---
+++ PerfectChange.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PerfectChange
+{
+    static class Greedy
+    {
+
+    }
+
+    class Dynamic
+    {
+
+    }
+    
+    public static void main(String[] args)
+    {
+        System.out.println("works");
+    }
+}
